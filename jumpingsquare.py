983c91e28a682845b2ac2c6698740e3e1e4374e7
bengunur06 <bengunur2@gmail.com>
2020-11-03 23:27:11 +0300

    jumping rectangle

--- jumpingsquare.py
+++ jumpingsquare.py
@@ -8,10 +8,14 @@ screenHeight = 500
 pygame.display.set_caption("Game window BNG")
 
 x = 50 
-y = 50
+y = 400
 width = 40
 height = 60
 ve = 5 
+
+isJump = False
+jumpCount = 10
+
 
 run = True 
 while run:
@@ -27,11 +31,24 @@ while run:
         x -= ve
     if keys[pygame.K_RIGHT] and  x < screenWidth - width  :
         x += ve 
-    if keys[pygame.K_UP] and y > ve  :
-        y -= ve
-    if keys[pygame.K_DOWN] and y < screenHeight - height:
-        y += ve
-    
+    if not (isJump):
+        if keys[pygame.K_UP] and y > ve  :
+            y -= ve
+        if keys[pygame.K_DOWN] and y < screenHeight - height:
+            y += ve
+        if keys[pygame.K_SPACE] :
+            isJump = True
+    else:
+        if jumpCount >= -10 :
+            neg = 1 
+            if jumpCount < 0 :
+                neg = -1
+            y -= ( jumpCount ** 2) * 0.5 * neg 
+            jumpCount-=1
+        else : 
+            isJump = False
+            jumpCount = 10
+
     win.fill((0,0,0))
     pygame.draw.rect(win,(255,0,0),(x,y,width ,height))
     pygame.display.update()
